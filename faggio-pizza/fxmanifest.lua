60b8e49ccbb92aa9d3fd571baaf6ddc050469cd3
Super Cool Ninja <samir932205@gmail.com>
2022-06-10 18:11:18 +0200

    Update fxmanifest.lua

--- faggio-pizza/fxmanifest.lua
+++ faggio-pizza/fxmanifest.lua
@@ -3,4 +3,7 @@ lua54 'yes'
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Super Cool Ninja'
+description 'this script allow u to spawn a faggio sport with the pizza box usefull for delivery'
+
 client_script 'main.lua'
